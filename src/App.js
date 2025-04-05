3af3a2ec7cc27c4fc139fa6dbcc9070097e9d371
Arun <arunby15@gmail.com>
2025-04-06 01:46:52 +0530

    Add Project Files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import { Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Cart from "./pages/Cart"
+
+const App = () => {
+  return (
+  <div>
+        <div className="bg-slate-900 fixed top-0 left-0 w-full shadow z-50">
+          <Navbar/>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/cart" element={<Cart/>} />
+        </Routes>
+
+  </div>)
+};
+
+export default App;
